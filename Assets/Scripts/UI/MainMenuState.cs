52ca3bb89433119105aaa4e9a93852f571e26d45
Mykyta <myk23ta@gmail.com>
2024-01-18 16:56:24 +0200

    Create main menu #2

---
+++ Assets/Scripts/UI/MainMenuState.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    class MainMenuState: MonoBehaviour
+    {
+        [SerializeField] private GameObject menu;
+        [SerializeField] private GameObject settingWindow;
+
+        private bool menuIsOpen;
+        private bool settingIsOpen;
+
+        private void Start()
+        {
+            menuIsOpen = true;
+            settingIsOpen = false;
+            menu.SetActive(menuIsOpen);
+            settingWindow.SetActive(settingIsOpen);
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (settingIsOpen && !menuIsOpen)
+                {
+                    CloseSettings();
+                }
+            }
+        }
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+        public void OpenSettings()
+        {
+            menuIsOpen = false;
+            settingIsOpen = true;
+            menu.SetActive(menuIsOpen);
+            settingWindow.SetActive(settingIsOpen);
+        }
+        public void CloseSettings()
+        {
+            menuIsOpen = true;
+            settingIsOpen = false;
+            menu.SetActive(menuIsOpen);
+            settingWindow.SetActive(settingIsOpen);
+        }
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
